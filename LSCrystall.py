71f63088274743463d4bce07cc91885c8e5ae894
Julia Salnikova <agereth@gmail.com>
2018-08-03 19:43:32 +0300

    refactored, docstrings added

--- LSCrystall.py
+++ LSCrystall.py
@@ -10,42 +10,65 @@ spreadsheetId = '1sLBlQ4CB5Jg_2u6PHM8TWyguRj8-oWZGTSkgAXmszFw'
 diode_indexes = {'алюминий':1, 'медь':2, 'green':3, 'red':4, 'royal blue': 5, 'orange': 6, 'arctic blue':7, 'deep red': 8, 'pc amber':9,
                  'white': 10}
 
-color_codes = {'green': ('gr', 'green', 'g'),
-               'red': ('red', 'r'),
-               'royal blue': ('rb', 'royal blue', 'royalblue'),
-               'orange':('o', 'orange'),
-               'arctic blue': ('ab', 'arctic', 'arcticblue', 'arctic blue'),
-               'deep red': ('dr', 'deepred', 'deep red'),
-               'pc amber': ('pc', 'amber', 'pcamber', 'pc amber'),
-               'white':('w', 'white')}
+color_picker = {'g':'green', 'gr':'green', 'green':'green',
+                'r': 'red', 'red': 'red',
+                'rb': 'royal blue', 'royalblue': 'royal blue', 'royal blue': 'royal blue',
+                'o': 'orange', 'orange':'orange',
+                'ab': 'arctic blue', 'arctic': 'arctic blue', 'arcticblue': 'arctic blue', 'arctic blue': 'arctic blue',
+                'deep red': 'deep red', 'dr':'deep red', 'deepred': 'deep red',
+                'pc amber': 'pc amber', 'pc': 'pc amber', 'pcamber':'pcamber',
+                'w':'white', 'white':'white'}
 
 threshold_board = 20
 threshold_crystal = 10
 
+def check_quantity(new_count: int, component: str,  threshold: int, component_type: str)->str:
+    """
+    check if new count of component is less then threshold (20 for boards, 10 for crystals)
+    :param new_count: current component quantity
+    :param component: type of crystal or board
+    :param threshold: threshold
+    :param component_type: boards ('подложки') or crystals 'кристаллы'
+    :return: nothing if there is enough, "МАЛО!" if 0 < new_count < threshold, "НЕТ!" if  new_count = 0
+    """
 
-def update_crystals(diode: list, values: list, now:datetime, service)->bool:
+    if new_count <= threshold:
+        if new_count == 0:
+            print('%s %s кончились!' % (component_type, component))
+            return 'НЕТ!'
+        print("%s %s кончаются, осталось %i" % (component_type, component, new_count))
+        return "МАЛО!"
+    return ""
+
+
+def update_crystals(diode: str, values: list, now: datetime, service: apiclient.discovery.Resource) -> bool:
     """
-    :param diode:
-    :return:
+    function for decreasing quantity of available crystals and boards
+    it gets string with diode information (three colours of crystals, type of board, quantity
+    and modifies all necessary rows in googlesheet
+
+    :param diode: string with type of dioide and quantity
+    :param values: quantity of diodes from spreadsheet
+    :param now: current date and time
+    :param service: service for work with  googlesheets
+    :return: true if updated successfully
     """
+
     data = diode.split()
     count = int(data[-1])
     board_type = data[-2]
     crystals = data[:-2]
 
-    row = values[diode_indexes[board_type]]
+    try:
+        row = values[diode_indexes[board_type]]
+    except IndexError:
+        print ("Wrong board type")
+        return False
+
     new_count = (int(row[1]) - count)
     row[1] = str(new_count)
     row[3] = row[3] + ', %s  %d.%d.%d' % (diode, now.day, now.month, now.year)
-    if new_count <= threshold_board:
-        if new_count == 0:
-            print('Подложки %s кончились!' % board_type)
-            row[2] = 'НЕТ!'
-        else:
-            print("Подложки %s кончаются, осталось %i" % (board_type, new_count))
-            row[2] = "МАЛО!"
-    else:
-        row[2] = ""
+    row[2] = check_quantity(new_count, board_type, threshold_board, "Подложки")
 
     request_body = {"valueInputOption": "RAW", "data": [{"range": "a%i:d%i" % (diode_indexes[board_type]+1, diode_indexes[board_type]+1),
                                                          "values": [row]}]}
@@ -57,46 +80,36 @@ def update_crystals(diode: list, values: list, now:datetime, service)->bool:
         colors =('red', 'green', 'royal blue')
     else:
         colors = []
-        crystals = (' ').join(crystals)
+        crystals = ' '.join(crystals)
         crystals = crystals.split('-')
         crystals = [crystal.strip() for crystal in crystals]
         for crystal in crystals:
-            for (color, codes) in color_codes.items():
-                if crystal in codes:
-                    colors.append(color)
-
-    if len(colors) < 3:
-        print ("Wrong diode %s" % data)
-
-
+            try:
+                colors.append(color_picker[crystal])
+            except IndexError:
+                print("Can't recognize %s crystal" % crystal)
+                return False
 
     for color in colors:
         row = values[diode_indexes[color]]
         new_count = (int(row[1]) - count)
         row[1] = str(new_count)
         row[3] = row[3] + ', %s  %d.%d.%d' % (diode, now.day, now.month, now.year)
-        if new_count <= threshold_crystal:
-            print("Кристаллы %s кончаются" % color)
-            row[2] = "МАЛО!"
-        else:
-            row[2] = ""
+        row[2] = check_quantity(new_count, color, threshold_crystal, "Кристаллы")
+
         request_body = {"valueInputOption": "RAW",
                         "data": [{"range": "a%i:d%i" % (diode_indexes[color]+1, diode_indexes[color]+1),
                                   "values": [row]}]}
         request = service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheetId, body=request_body)
         _ = request.execute()
 
-
-
-
-
-
+    return True
 
 
 def main (diodes: str):
     """
-
-    :param file:
+    updates spreadsheet with crystals and boards quantity
+    :param diodes: list of diodes
     :return:
     """
 
